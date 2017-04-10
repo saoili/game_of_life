0805a7f4d5b615a895efca1c529339c686f1f637
saoili <saoili@gmail.com>
2017-04-10 22:55:04 +0100

    added more unit tests

--- test_toroid_game_of_life.py
+++ test_toroid_game_of_life.py
@@ -58,6 +58,36 @@ class TestGameOfLifeGrid(TestCase):
         for key in g.grid:
             self.assertEqual(g.total_around(*key), expected[key])
 
+    def test_next_value(self):
+        # tests where the current field is alive
+        g = GameOfLife(3)
+        g.grid[(1, 1)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # underpopulation
+        g.grid[(0, 0)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # underpopulation
+        g.grid[(0, 1)] = 1
+        self.assertEqual(g.next_value(1, 1), 1)  # survives
+        g.grid[(0, 2)] = 1
+        self.assertEqual(g.next_value(1, 1), 1)  # survives
+        g.grid[(1, 0)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # overpopulation
+
+        # tests where the current field is not alive
+        g = GameOfLife(3)
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+        g.grid[(1, 1)] = 0
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+        g.grid[(0, 0)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+        g.grid[(0, 1)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+        g.grid[(0, 2)] = 1
+        self.assertEqual(g.next_value(1, 1), 1)  # new life!
+        g.grid[(1, 0)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+        g.grid[(1, 2)] = 1
+        self.assertEqual(g.next_value(1, 1), 0)  # no new life
+
 
 if __name__ == "__main__":
     main()
